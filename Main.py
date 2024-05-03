9bfd34245f89513e09fbb2974736f48fe2055f18
MasterOfN0ne <156803188+MasterOfN0ne@users.noreply.github.com>
2024-05-03 08:13:32 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -61,3 +61,81 @@ def finalTable(a, b, A, B, Sa, Sb):
 
 finalTable(a, b, A, B, shared_secret_key_A, shared_secret_key_B)
 
+inputbinlist = []
+keybinlist = []
+result_list = []
+result = []
+
+EOD = int(input("Encrypt(1) or Decrypt(2)?: "))
+if EOD == 1:
+    user_input = input("Enter the text you would like to encrypt: ")
+    key_input = input("Enter the key of the encrypted text: ")
+
+    def iterate_to_match_length(short_str, long_str):
+        short_len = len(short_str)
+        long_len = len(long_str)
+        iterations = long_len // short_len
+        remainder = long_len % short_len
+
+        repeated_str = short_str * iterations + short_str[:remainder]
+        return repeated_str
+
+
+    key_iterate1 = iterate_to_match_length(key_input, user_input)
+
+    for c in user_input:
+        input_binary = format(ord(c), '08b')  # Convert character to 8-bit binary string
+        inputbinlist.append(input_binary)
+
+    for c in key_iterate1:
+        input_key = format(ord(c), '08b')  # Convert character to 8-bit binary string
+        keybinlist.append(input_key)
+
+
+    def xor_lists(list1, list2):
+
+        for i in range(min(len(list1), len(list2))):
+            result.append(''.join(str(int(x) ^ int(y)) for x, y in zip(list1[i], list2[i])))
+        return result
+
+
+    xor_result = xor_lists(inputbinlist, keybinlist)
+    print("XOR result list:", xor_result)
+elif EOD == 2:
+    bin_key = []
+    enc_txt_bin = []
+    dec_key = input("Enter the key for decryption: ")
+    enc_txt = input("Enter the encrypted text: ")
+
+
+    def iterate_to_match_length(short_str, long_str):
+        short_len = len(short_str)
+        long_len = len(long_str)
+        iterations = long_len // short_len
+        remainder = long_len % short_len
+
+        repeated_str = short_str * iterations + short_str[:remainder]
+        return repeated_str
+
+    key_iterate = iterate_to_match_length(dec_key, enc_txt)
+
+    for c in key_iterate:
+        key_binary = format(ord(c), '08b')  # Convert character to 8-bit binary string
+        bin_key.append(key_binary)
+    for a in enc_txt:
+        txt_binary = format(ord(a), '08b')  # Convert character to 8-bit binary string
+        enc_txt_bin.append(txt_binary)
+
+    def xor_lists(list1, list2):
+        result2 = []
+        for i in range(min(len(list1), len(list2))):
+            result2.append(''.join(str(int(x) ^ int(y)) for x, y in zip(list1[i], list2[i])))
+        return result2
+
+
+    xor_finalfinal = xor_lists(bin_key, enc_txt_bin)
+    dec_chars = ''.join(chr(int(y, 2)) for y in xor_finalfinal)
+    print(dec_chars)
+
+else:
+    print("Invalid option. Please enter 1 for encryption or 2 for decryption.")
